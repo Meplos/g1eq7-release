33e6406adbcca93437e50305c98eee9c5935bfa2
aladoux <alixjose64@gmail.com>
2020-11-12 12:21:04 +0100

    debut nf11

--- frontend/src/components/Project.vue
+++ frontend/src/components/Project.vue
@@ -1,9 +1,49 @@
 <template>
-  <h1>Hello World! I'm project</h1>
+  <div class="projectVue">
+    <v-row>
+      <h1>{{title}}dede</h1>
+    </v-row>
+    <v-row col="12" sm="8">
+      <label>Date de début: </label>
+    </v-row>
+    <v-row col="12" sm="8">
+      <label>{{dateDeb}}</label>
+    </v-row>
+    <v-row col="12" sm="8">
+      <label>Etat:</label>
+    </v-row>
+    <v-row col="12" sm="8">
+      <label>{{etat}}</label>
+    </v-row>
+    <v-row col="12" sm="8">
+      <label v-if="haveDateFin">Date de fin estimée:</label>
+    </v-row>
+    <v-row col="12" sm="8">
+      <label>{{dateFinEst}}</label>
+    </v-row>
+    <v-row col="12" sm="8">
+      <label v-if="haveDepot">Dépot:</label>
+    </v-row>
+    <v-row col="12" sm="8">
+      <label>{{depot}}</label>
+    </v-row>
+    <v-row col="12" sm="8">
+      <label v-if="haveDesc">Description:</label>
+    </v-row>
+    <v-row col="12" sm="8">
+      <label>{{desc}}</label>
+    </v-row>
+  </div>
+  
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    id: Number
+  },
+  
+};
 </script>
 
 <style></style>
