691b79d11a1920ea343d1923fe1463b46a452fa5
aerard <alexxd64150@gmail.com>
2020-11-12 11:22:58 +0100

    axios interceptors

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -13,6 +13,16 @@ axios.defaults.headers = {
   },
 };
 
+axios.interceptors.request.use((request) => {
+  console.log("Starting Request", JSON.stringify(request, null, 2));
+  return request;
+});
+
+axios.interceptors.response.use((response) => {
+  console.log("Response:", JSON.stringify(response, null, 2));
+  return response;
+});
+
 Vue.config.productionTip = false;
 
 Vue.use(VueFilterDateFormat);
