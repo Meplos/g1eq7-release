8bc3d6a3c5cc9480f52f7ffd42baaec9a5b1ed2e
aerard <alexxd64150@gmail.com>
2020-11-17 23:37:25 +0100

    NF25 Implém. du drag&drop des us dans des sprint

---
+++ frontend/src/components/cards/USCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="usCard__container">
+    <h1>{{ us.id }}</h1>
+    <p>{{ us.description }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    us: Object,
+  },
+  mounted() {},
+};
+</script>
+
+<style>
+.usCard__container {
+  width: 145px;
+  height: 195px;
+  margin: 6px;
+  background: #413d3d;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.25);
+  border-radius: 33px;
+}
+</style>
