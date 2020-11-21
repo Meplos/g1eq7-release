a73000b850101401fdcd9dbc46cd9b720cb163ad
max98hash <maxime8198@gmail.com>
2020-11-21 15:47:51 +0100

    T9_1 Done

---
+++ backend/test/US2/t9_1.js
@@ -0,0 +1,41 @@
+const fetch = require("node-fetch");
+
+const webdriver = require('selenium-webdriver'),
+    By = webdriver.By,
+    until = webdriver.until;
+
+const driver = new webdriver.Builder()
+    .forBrowser('chrome')
+    .build();
+
+let previousUSCount = 0;
+
+console.log("Test création US");
+
+driver.get("http://localhost:8080")
+.then(() => driver.sleep(1000))
+.then(() => driver.findElement(By.className("projectCard v-card v-sheet theme--dark elevation-2")))
+.then((project) => project.click())
+.then(() => driver.sleep(1000))
+.then(() => driver.findElements(By.css("tr")))
+.then((lignes) => previousUSCount=lignes.length)
+.then(() => driver.findElement(By.className("mx-2 success v-btn v-btn--contained v-btn--fab v-btn--round theme--dark v-size--default")))
+.then((button) => button.click())
+.then(() => {
+    let desc = driver.findElement(By.id("input-52"));
+    desc.sendKeys("As a developper I wan't to add and US so I can manage them");
+})
+.then(() => driver.findElement(By.className("mr-6 v-btn v-btn--contained theme--dark v-size--default success")))
+.then((button) => button.click())
+.then(() => driver.sleep(1000))
+.then(() => driver.findElements(By.css("tr")))
+.then((lignes) => {
+    if(lignes.length==previousUSCount+1){
+        console.log("ok");
+    }
+    else{
+        console.log("non");
+    }
+})
+.then(() => driver.quit());
+
