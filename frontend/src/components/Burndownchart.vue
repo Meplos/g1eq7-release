22671bb8a3c1c951f3bf7d3829b7fe70cd1c1050
aladoux <alixjose64@gmail.com>
2020-11-25 15:40:44 +0100

    NF30 start/ ajout du bouton generer brundown chart et création de burndownchart.vue

---
+++ frontend/src/components/Burndownchart.vue
@@ -0,0 +1,15 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
+
+<script>
+
+//sum elements of an array up to the index provided
+function sumArrayUpTo(arrData, index) {
+    var total = 0;
+    for(var i = 0; i<=index; i++){
+        if(arrData.lenght > i) {
+            total += arrData[i];
+        }
+    }
+    return total;
+}
+</script>
